f1eb2e2e37007043e8859b1f085a5c1e8ef49b7b
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-04-12 02:54:23 +0330

    fix some minor bug

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from collections import defaultdict
 from enum import Enum, auto
 
-keywords = {'break', 'else', 'if', 'int', 'repeat', 'return', 'until', 'void'}
-single_symbols = {'+', '-', '<', ':', ';', ',', '(', ')', '[', ']', '{', '}'}
-slash_symbol = {'/'}
-star_symbol = {'*'}
-equal_symbol = {'='}
-whitespaces = {' ', '\n', '\r', '\t', '\v', '\f'}
-digits = {chr(i) for i in range(48, 58)}
-letters = {chr(i) for i in range(65, 91)}.union({chr(i) for i in range(97, 123)})
+keywords = ['break', 'else', 'if', 'int', 'repeat', 'return', 'until', 'void']
+single_symbols = ['+', '-', '<', ':', ';', ',', '(', ')', '[', ']', '{', '}']
+slash_symbol = ['/']
+star_symbol = ['*']
+equal_symbol = ['=']
+whitespaces = [' ', '\n', '\r', '\t', '\v', '\f']
+digits = [chr(i) for i in range(48, 58)]
+letters = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+legal_chars = single_symbols + slash_symbol + star_symbol + equal_symbol + whitespaces + digits + letters + [None]
+illegal_chars = [chr(i) for i in range(256) if chr(i) not in legal_chars]
 
 
-class StateType(Enum):
+class TokenType(Enum):
     NUM = auto()
     ID = auto()
     KEYWORD = auto()
@@ -23,16 +24,7 @@ class StateType(Enum):
     EOF = auto()
 
 
-class TokenType(Enum):
-    PANIC = auto()
-    KEYWORD = auto()
-    IDENTIFIER = auto()
-    NUMBER = auto()
-    SYMBOL = auto()
-    EOF = auto()
-
-
-hidden_tokens = {StateType.COMMENT, StateType.WHITESPACE, StateType.START, StateType.PANIC, StateType.EOF}
+hidden_tokens = [TokenType.COMMENT, TokenType.WHITESPACE, TokenType.START, TokenType.PANIC, TokenType.EOF]
 
 
 def line_number_str(line_number):
@@ -41,56 +33,40 @@ def line_number_str(line_number):
 
 class SymbolTable:
     def __init__(self):
-        self.symbols = set(keywords)
+        self.symbols = dict.fromkeys(keywords)
 
     def add_symbol(self, symbol):
-        self.symbols.add(symbol)
-
-    def __contains__(self, symbol):
-        return symbol in self.symbols
+        self.symbols[symbol] = None
 
     def __str__(self):
-        return "\n".join(f'{line_number_str(i + 1)}{symbol}' for i, symbol in enumerate(sorted(self.symbols)))
+        s = ''
+        for i, symbol in enumerate(self.symbols):
+            s += f'{line_number_str(i + 1)}{symbol}\n'
+        return s
 
 
 class Reader:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, file):
         self.line_number = 0
-        self.file = None
-        self.line = ""
-        self.char_index = 0
-
-    def __enter__(self):
-        try:
-            self.file = open(self.file_path, 'r')
-        except IOError as e:
-            print(f"Failed to open file: {e}")
-            # Raise the exception to prevent further execution
-            raise
+        self.file = file
         self.line = self.readline()
         self.char_index = 0
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.file:
-            self.file.close()
 
     def get_char(self):
         if self.char_index >= len(self.line):
             self.line = self.readline()
             self.char_index = 0
-            if self.line == "":
-                return None
-        char = self.line[self.char_index]
+        if not self.line:
+            return None
+        c = self.line[self.char_index]
         self.char_index += 1
-        return char
+        return c
 
     def readline(self):
         line = self.file.readline()
         if line:
             self.line_number += 1
-        return line.rstrip('\n')
+        return line
 
 
 class Token:
@@ -101,147 +77,189 @@ class Token:
     def __str__(self):
         return f'({self.token_type}, {self.token_value})'
 
-    def __repr__(self):
-        return f'Token({self.token_type!r}, {self.token_value!r})'
-
 
 class State:
     states = {}
 
-    def __init__(self, state_id, state_type, is_final=False, is_star=False, error=''):
-        self.default_transition = None
+    def __init__(self, state_id, state_type, is_final, is_star, error=''):
         self.id = state_id
         self.transitions = {}
         self.error = error
         self.state_type = state_type
         self.is_final = is_final
         self.is_star = is_star
-        State.states[state_id] = self
+        self.states[state_id] = self
 
-    @classmethod
-    def create_state(cls, state_id, state_type, is_final=False, is_star=False, error=''):
-        return cls(state_id, state_type, is_final, is_star, error)
-
-    def add_transition(self, characters, destination_state_id):
-        for char in characters:
-            self.transitions[char] = State.states[destination_state_id]
+    def add_transition(self, characters, destination):
+        self.transitions.update(dict.fromkeys(characters, destination))
         return self
 
-    def add_default_transition(self, destination_state_id):
-        self.default_transition = State.states.get(destination_state_id)
+    def otherwise(self, destination):
+        self.add_transition([chr(i) for i in range(256) if chr(i) not in self.transitions], destination)
+        if None not in self.transitions:
+            self.transitions[None] = destination
 
-    def next_state(self, char):
-        return self.transitions.get(char, self.default_transition)
+    def next_state(self, next_char):
+        return self.transitions.get(next_char)  # returns None if char is not in transitions
 
 
 class Scanner:
     def __init__(self, reader: Reader, start_state: State):
         self.symbol_table = SymbolTable()
         self.reader = reader
-        self.start_state = start_state
-        self.current_state = start_state
-        self.tokens = defaultdict(list)
-        self.lexical_errors = defaultdict(list)
+        self.start_state: State = start_state
+        self.current_state: State = start_state
+        self.tokens = {}  # line_number: list of tokens
+        self.lexical_errors = {}  # line_number: list of errors
 
     def get_next_token(self) -> Token:
         token_name = ''
-        char = self.reader.get_char()
-        while char is not None:
-            next_state = self.current_state.next_state(char)
-            if next_state is None:
-                # Handle the case where no valid transition exists.
-                # For example, transition to a PANIC state or return an error token.
-                return Token(TokenType.PANIC, "Invalid character sequence")
-            self.current_state = next_state
-            if not self.current_state.is_star:
-                token_name += char
-            char = self.reader.get_char() if not self.current_state.is_star else char
-        if self.current_state.is_final:
-            if self.current_state.state_type == StateType.ID and token_name in keywords:
-                return Token(TokenType.KEYWORD, token_name)
+        while not self.current_state.is_final:
+            c = self.reader.get_char()
+            self.current_state = self.current_state.next_state(next_char=c)
+            if self.current_state.is_star:
+                self.reader.char_index -= 1
             else:
-                return Token(self.current_state.state_type, token_name)
-        return Token(TokenType.EOF, '')
+                token_name += c
+
+        if self.current_state.state_type == TokenType.ID:
+            self.symbol_table.add_symbol(token_name)
+            if token_name in keywords:
+                return Token(TokenType.KEYWORD, token_name)
+        elif self.current_state.state_type == TokenType.PANIC:
+            token_name = token_name[:7] + '...' if len(token_name) > 7 else token_name
+        return Token(self.current_state.state_type, token_name)
 
     def get_tokens(self):
-        while True:
+        token = Token(token_type=TokenType.START)
+        while token.token_type != TokenType.EOF:
             self.current_state = self.start_state
             line_number = self.reader.line_number
             token = self.get_next_token()
 
-            if token.token_type == TokenType.EOF:
-                break
-
             if token.token_type == TokenType.PANIC:
-                self.lexical_errors[line_number].append(token)
-            else:
-                self.tokens[line_number].append(token)
+                self.lexical_errors.setdefault(line_number, []) \
+                    .append(Token(token.token_value, self.current_state.error))
+            self.tokens.setdefault(line_number, []).append(token)
 
     def __str__(self):
-        output = ''
-        for line_number, tokens in self.tokens.items():
-            line_tokens = ' '.join(str(token) for token in tokens if token.token_type not in hidden_tokens)
+        s = ''
+        for line_number in self.tokens:
+            line_tokens = ''
+            for token in self.tokens[line_number]:
+                if token.token_type not in hidden_tokens:
+                    line_tokens += str(token) + ' '
             if line_tokens:
-                output += f'{line_number_str(line_number)}{line_tokens}\n'
-        return output
+                s += line_number_str(line_number) + line_tokens + '\n'
+        return s
 
     def repr_lexical_errors(self):
         if not self.lexical_errors:
-            return 'There are no lexical errors.'
-        return '\n'.join(f'{line_number_str(line)}' + ' '.join(str(token) for token in tokens)
-                         for line, tokens in self.lexical_errors.items())
+            return 'There is no lexical error.'
+        s = ''
+        for line_number in self.lexical_errors:
+            s += line_number_str(line_number)
+            for token in self.lexical_errors[line_number]:
+                s += str(token) + ' '
+            s += '\n'
+        return s
 
 
 def initialize_states():
-    # Creating states
-    State.create_state(0, StateType.START, is_final=False, is_star=False)
+    State(0, TokenType.START, is_final=False, is_star=False)
 
-    State.create_state(10, StateType.NUM, is_final=False, is_star=False)
-    State.create_state(11, StateType.NUM, is_final=True, is_star=True)
+    State(10, TokenType.NUM, is_final=False, is_star=False)
+    State(11, TokenType.NUM, is_final=True, is_star=True)
 
-    State.create_state(20, StateType.ID, is_final=False, is_star=False)
-    State.create_state(21, StateType.ID, is_final=True, is_star=True)
+    State(20, TokenType.ID, is_final=False, is_star=False)
+    State(21, TokenType.ID, is_final=True, is_star=True)
 
-    State.create_state(30, StateType.WHITESPACE, is_final=False, is_star=False)
-    State.create_state(31, StateType.WHITESPACE, is_final=True, is_star=True)
+    State(30, TokenType.WHITESPACE, is_final=False, is_star=False)
+    State(31, TokenType.WHITESPACE, is_final=True, is_star=True)
 
-    State.create_state(40, StateType.SYMBOL, is_final=True, is_star=False)  # Always-single symbols
-    State.create_state(41, StateType.SYMBOL, is_final=False, is_star=False)  # Equal symbol reached
-    State.create_state(42, StateType.SYMBOL, is_final=True, is_star=False)  # Double equal finished
-    State.create_state(43, StateType.SYMBOL, is_final=True,
-                       is_star=True)  # Reached other characters after single/double-symbol
+    State(40, TokenType.SYMBOL, is_final=True, is_star=False)  # Always-single symbols
+    State(41, TokenType.SYMBOL, is_final=False, is_star=False)  # Equal symbol reached
+    State(42, TokenType.SYMBOL, is_final=True, is_star=False)  # Double equal finished
+    State(43, TokenType.SYMBOL, is_final=True, is_star=True)  # Reached other characters after single/double-symbol
 
-    State.create_state(50, StateType.COMMENT, is_final=False, is_star=False)  # '/' reached
-    State.create_state(51, StateType.COMMENT, is_final=False, is_star=False)  # '*' reached after '/' (comment)
-    State.create_state(52, StateType.COMMENT, is_final=False, is_star=False)  # '*' reached inside comment
-    State.create_state(53, StateType.COMMENT, is_final=True, is_star=False)  # '/' reached after '*' (comment finished)
-    State.create_state(54, StateType.COMMENT, is_final=False, is_star=False)  # '*' reached outside comment
+    State(50, TokenType.COMMENT, is_final=False, is_star=False)  # '/' reached
+    State(51, TokenType.COMMENT, is_final=False, is_star=False)  # '*' reached after '/' (comment)
+    State(52, TokenType.COMMENT, is_final=False, is_star=False)  # '*' reached inside comment
+    State(53, TokenType.COMMENT, is_final=True, is_star=False)  # '/' reached after '*' (comment finished)
+    State(54, TokenType.COMMENT, is_final=False, is_star=False)  # '*' reached outside comment
 
-    State.create_state(90, StateType.PANIC, is_final=True, is_star=False, error='Invalid number')
-    State.create_state(92, StateType.PANIC, is_final=True, is_star=True, error='Unclosed comment')
-    State.create_state(93, StateType.PANIC, is_final=True, is_star=False, error='Invalid input')
-    State.create_state(94, StateType.PANIC, is_final=True, is_star=True, error='Invalid input')
+    State(90, TokenType.PANIC, is_final=True, is_star=False, error='Invalid number')
+    State(92, TokenType.PANIC, is_final=True, is_star=True, error='Unclosed comment')
+    State(93, TokenType.PANIC, is_final=True, is_star=False, error='Invalid input')
+    State(94, TokenType.PANIC, is_final=True, is_star=True, error='Invalid input')
 
-    State.create_state(95, StateType.PANIC, is_final=True, is_star=False, error='Unmatched comment')
+    State(95, TokenType.PANIC, is_final=True, is_star=False, error='Unmatched comment')
 
-    State.create_state(100, StateType.EOF, is_final=True, is_star=True)
+    State(100, TokenType.EOF, is_final=True, is_star=True)
 
+    State.states[0] \
+        .add_transition(digits, State.states[10]) \
+        .add_transition(letters, State.states[20]) \
+        .add_transition(whitespaces, State.states[30]) \
+        .add_transition([None], State.states[100]) \
+        .add_transition(single_symbols, State.states[40]) \
+        .add_transition(equal_symbol, State.states[41]) \
+        .add_transition(slash_symbol, State.states[50]) \
+        .add_transition(star_symbol, State.states[54]) \
+        .add_transition(illegal_chars, State.states[93]) \
+        .otherwise(State.states[93])
 
-def main(file_name):
-    initialize_states()
-    try:
-        with Reader(file_name) as reader:
-            scanner = Scanner(reader, State.states[0])
-            scanner.get_tokens()
-        with open('tokens.txt', 'w') as output_file:
-            output_file.write(str(scanner))
-        with open('symbol_table.txt', 'w') as output_file:
-            output_file.write(str(scanner.symbol_table))
-        with open('lexical_errors.txt', 'w') as output_file:
-            output_file.write(scanner.repr_lexical_errors())
-    except FileNotFoundError:
-        print(f"File {file_name} not found.")
+    State.states[10] \
+        .add_transition(digits, State.states[10]) \
+        .add_transition(letters, State.states[90]) \
+        .otherwise(State.states[11])
+
+    State.states[20] \
+        .add_transition(digits + letters, State.states[20]) \
+        .add_transition(illegal_chars, State.states[93]) \
+        .otherwise(State.states[21])
+
+    State.states[30] \
+        .add_transition(whitespaces, State.states[30]) \
+        .otherwise(State.states[31])
+
+    State.states[41] \
+        .add_transition(equal_symbol, State.states[42]) \
+        .add_transition(illegal_chars, State.states[93]) \
+        .otherwise(State.states[43])
+
+    State.states[50] \
+        .add_transition(legal_chars, State.states[94]) \
+        .add_transition(star_symbol, State.states[51]) \
+        .otherwise(State.states[93])
+
+    State.states[51] \
+        .add_transition(star_symbol, State.states[51]) \
+        .add_transition([None], State.states[92]) \
+        .add_transition(star_symbol, State.states[52]) \
+        .otherwise(State.states[51])
+
+    State.states[52] \
+        .add_transition(slash_symbol, State.states[53]) \
+        .add_transition([None], State.states[92]) \
+        .otherwise(State.states[51])
+
+    State.states[54] \
+        .add_transition(slash_symbol, State.states[95]) \
+        .add_transition(illegal_chars, State.states[93]) \
+        .otherwise(State.states[43])
 
 
 if __name__ == '__main__':
-    main('input.txt')
+    file_name = "input.txt"
+    initialize_states()
+
+    with open(file_name, 'r') as input_file:
+        scanner = Scanner(reader=Reader(input_file), start_state=State.states[0])
+        scanner.get_tokens()
+    with open('tokens.txt', 'w') as output_file:
+        output_file.write(str(scanner))
+    with open('symbol_table.txt', 'w') as output_file:
+        output_file.write(str(scanner.symbol_table))
+    with open('lexical_errors.txt', 'w') as output_file:
+        output_file.write(scanner.repr_lexical_errors())
