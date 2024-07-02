de44268f20e46ea805128e914ce439c32511768d
Soroush Sherafat <sorousherafat@gmail.com>
2024-07-02 14:05:19 +0330

    better error messages for test script

--- run-single-test.sh
+++ run-single-test.sh
@@ -10,21 +10,21 @@ if grep -q "$NO_OUTPUT_GENERATED" output.txt;
 then
     if ! diff output.txt "$test/output.txt" >> /dev/null 2>&1;
     then
-        echo "$test failed"
+        echo "$test failed. semantic errors detected. 'output.txt' is different."
     fi
     if ! diff semantic_errors.txt "$test/semantic_errors.txt" >> /dev/null 2>&1;
     then
-        echo "$test failed"
+        echo "$test failed. semantic errors detected. 'semantic_errors.txt' is different."
     fi
 else
     interpreter/tester_linux.out output.txt > actual.txt 2> /dev/null
     sed -i '/^Total/d' actual.txt
     if ! diff actual.txt "$test/expected.txt" >> /dev/null 2>&1;
     then
-        echo "$test failed"
+        echo "$test failed. code generated. 'actual.txt' is different than 'expected.txt'."
     fi
     if ! diff semantic_errors.txt "$test/semantic_errors.txt" >> /dev/null 2>&1;
     then
-        echo "$test failed"
+        echo "$test failed. code generated. 'semantic_errors.txt' is different."
     fi
 fi
